cc67903b255adecf70401915698b6a3d1c9bd7fe
danieldil <edoloman@me.com>
2016-04-04 19:57:32 -0700

    implemented a working huffmancode function

--- src/huffmanCode/HuffmanCode.java
+++ src/huffmanCode/HuffmanCode.java
@@ -1,9 +1,9 @@
 package huffmanCode;
 import java.util.*;
 public class HuffmanCode implements Comparable<HuffmanCode>{
-	char letter;
+	String letter;
 	double prob;
-	public HuffmanCode(char iletter, double iprob){
+	public HuffmanCode(String iletter, double iprob){
 		letter = iletter;
 		prob = iprob;
 	}
@@ -11,7 +11,7 @@ public class HuffmanCode implements Comparable<HuffmanCode>{
 		return "\nletter:"+ this.letter + " probability: "+this.prob+ "\n";
 	}
 	public boolean isLessThan(HuffmanCode s){
-		if(prob<s.prob){
+		if(prob < s.prob){
 			return true;
 		}
 		else{
@@ -19,28 +19,63 @@ public class HuffmanCode implements Comparable<HuffmanCode>{
 		}
 	}
 	public void swap(HuffmanCode s){
-		char temp = letter;
+		String temp = letter;
 		double tempd = prob;
 		letter = s.letter;
 		prob = s.prob;
 		s.letter = temp;
 		s.prob = tempd;
 	}
+	public static void add2smallest(LinkedList<HuffmanCode> s,ArrayList<Node> nArray){
+		Collections.sort(s);
+		if(s.size() >= 2){
+			Node head;
+			Node left = new Node(s.get(1),null,null);
+			Node right = new Node(s.get(0),null,null);
+			head = new Node(null,left,right);
+			nArray.add(head);
+			System.out.println(head);
+			HuffmanCode add = 
+			new HuffmanCode(s.get(1).letter + s.get(0).letter,s.get(1).prob+s.get(0).prob);
+			s.remove(0);
+			s.remove(0);
+			//System.out.println(s);
+			s.addFirst(add);
+			//System.out.println(s);
+			System.out.println(nArray);
+		}
+		else{
+			System.out.println("too small of a list");
+		}
+	}
 public static void main(String[] args){
+	ArrayList<Node> nArray = new ArrayList<Node>();
 	LinkedList<HuffmanCode> hList = new LinkedList<HuffmanCode>();
-	hList.add(new HuffmanCode('a',.22));
-	hList.add(new HuffmanCode('b',.66));
-	hList.add(new HuffmanCode('c',.12));
+	hList.add(new HuffmanCode("a",.220));
+	hList.add(new HuffmanCode("b",.660));
+	hList.add(new HuffmanCode("c",.150));
+	hList.add(new HuffmanCode("d",.290));
+	hList.add(new HuffmanCode("e",.610));
+	hList.add(new HuffmanCode("f",.100));
+	hList.add(new HuffmanCode("g",.299));
+	hList.add(new HuffmanCode("h",.860));
+	hList.add(new HuffmanCode("i",.920));
 	
-	System.out.println("before sort " +hList);
-	Collections.sort(hList);
-	System.out.println("after "+ hList);
+	while(hList.size() > 2)
+	add2smallest(hList,nArray);
+	
+	for(int i=0;i<nArray.size();i++){
+		System.out.println(nArray.indexOf(i));
+	}
+	
+	
+	
 }
 @Override
 public int compareTo(HuffmanCode o) {
 	if(this.prob == o.prob)
 	return 0;
-	if(this.prob>o.prob){
+	if(this.prob > o.prob){
 		return 1;}
 	else
 		return -1;
--- src/huffmanCode/Node.java
+++ src/huffmanCode/Node.java
@@ -4,9 +4,24 @@ public class Node {
 	public HuffmanCode data;
 	public Node left;
 	public Node right;
+	public Node(HuffmanCode d, Node l, Node r){
+		data = d;
+		left = l;
+		right =r;
+	}
 	public void set(HuffmanCode d, Node l, Node r){
 		data = d;
 		left = l;
 		right =r;
+	}
+	public String toString(){
+		String s;
+		if(this.right != null){
+			s = String.valueOf((this.left.data.prob + "   " + this.right.data.prob));
+		}
+		else{
+			s = String.valueOf(this.left.data.prob);
+		}
+		return s;
 	}
 }
